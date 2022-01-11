638c951fca086db6287e8be6e857ab577a94be07
NilsMillot <compte.mxs@hotmail.fr>
2022-01-11 10:37:05 +0100

    la ci doit fail car les tests sont faux

--- test/item.test.ts
+++ test/item.test.ts
@@ -15,7 +15,7 @@ describe("Item validity", () => {
     it("Should be valid when the content size is 1000 characters", () => {
         const content1000Characters = "Lore ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ullamcorper ante diam, non placerat eros fringilla quis. Ut lorem ante, ultrices et rhoncus et, luctus sed magna. Proin placerat vel nulla ac egestas. Phasellus convallis, arcu in iaculis elementum, elit massa semper ante, in vestibulum lorem urna quis odio. Vivamus sed elit in ligula malesuada volutpat sit amet non ex. Aenean mollis purus ac justo viverra, nec porta sem scelerisque. Maecenas volutpat orci vitae leo eleifend pellentesque. Ut pharetra sodales suscipit. Vestibulum fermentum est sit amet nunc varius egestas. Nam felis elit, tempor ut varius non, rhoncus non nunc. Donec diam dolor, lobortis sed scelerisque et, accumsan eget eros. Etiam sed rhoncus diam, at ultrices turpis. Curabitur maximus sem arcu, quis scelerisque eros porttitor quis. Curabitur quam eros, fermentum ac lectus vel, pulvinar vestibulum elit.Sed vel gravida erat. Nulla vulputate tellus quis nisi facilisis, et condimentum velit aliquet donec."
         const item = new Item("task 1", content1000Characters);
-        expect(item.isValid()).toBe(true);
+        expect(item.isValid()).toBe(false);
     })
 
     it("Should be invalid when the content size is more than 1000 characters", () => {
